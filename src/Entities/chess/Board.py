3899182920a0c1aa5c5d59bb5a569016a192a689
Fabrício <fabricio_passadore@hotmail.com>
2021-11-08 01:17:14 -0300

    type hint correction

--- src/Entities/chess/Board.py
+++ src/Entities/chess/Board.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Dict
+from typing import Dict, Optional
 
 
 from src.Entities.chess.Position import Position
@@ -8,15 +8,16 @@ from src.Entities.chess.ChessPieces.AbstractPiece import AbstractPiece
 
 @dataclass
 class Board:
-    state: Dict[Position, AbstractPiece]
+    state: Dict[Position, Optional[AbstractPiece]]
+    king_positions: Dict[str, Position]
 
-    def set_piece_by_indexes(self, x: int, y: int, piece: AbstractPiece):
+    def set_piece_by_indexes(self, x: int, y: int, piece: Optional[AbstractPiece]):
         self.state[Position(x, y)] = piece
 
     def get_piece_by_indexes(self, x: int, y: int):
         return self.state[Position(x, y)]
 
-    def set_piece_by_position(self, position: Position, piece: AbstractPiece):
+    def set_piece_by_position(self, position: Position, piece: Optional[AbstractPiece]):
         self.state[position] = piece
 
     def get_piece_by_position(self, position: Position):
