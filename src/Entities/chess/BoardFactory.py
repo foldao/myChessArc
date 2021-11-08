4c26aa9d830a4ed5ae8662cad96499724e186a7c
Fabrício <fabricio_passadore@hotmail.com>
2021-11-08 01:17:40 -0300

    type hint corrections

--- src/Entities/chess/BoardFactory.py
+++ src/Entities/chess/BoardFactory.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Tuple, Union
+from typing import Dict, List, Optional
 from src.Entities.chess.Position import Position
 from src.Entities.chess.Board import Board
 from src.Entities.chess.ChessPieces.AbstractPiece import AbstractPiece
@@ -7,7 +7,7 @@ from src.Entities.chess.ChessPieces.AbstractPiece import AbstractPiece
 class BoardFactory:
     schema: Dict[str, List[str]]
 
-    def _validate_board(self, schema: str | Dict[str, List[str]]) -> Dict[str, List[str]]:
+    def _validate_board(self, schema: str | Dict[str, List[str]]):
         match schema:
             case str():
                 self.schema = self._create_board_dict_from_string(schema)
@@ -19,11 +19,11 @@ class BoardFactory:
 
     @staticmethod
     def create_empty_board() -> Board:
-        state = {Position(i, j): None for i in range(8) for j in range(8)}
-        return Board(state)
+        state:Dict[Position,Optional[AbstractPiece]] = {Position(i, j): None for i in range(8) for j in range(8)}
+        return Board(state,{})
 
     def get_board(self, schema: str | Dict[str, List[str]]) -> Board:
-        pass
+        raise NotImplementedError
 
     def _create_board_dict_from_string(self, board_json: str) -> Dict[str, List[str]]:
-        pass
+        raise NotImplementedError
