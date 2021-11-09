27c84b0edd7838909bec24b2ae0b510d9de7e17b
Fabrício <fabricio_passadore@hotmail.com>
2021-11-09 01:23:12 -0300

    new test case

--- tests/bishop_test.py
+++ tests/bishop_test.py
@@ -4,6 +4,13 @@ from src.Entities.chess.PieceColorEnum import PieceColorEnum
 from src.Entities.chess.Position import Position
 
 
+def test_valid_far_position():
+    board = BoardFactory.create_empty_board()
+    white_bishop = Bishop(PieceColorEnum.WHITE, 'Bishop')
+    board.set_piece_by_indexes(2, 4, white_bishop)
+    moves = white_bishop.get_possible_moves(board, Position(2, 4))
+    assert Position(5, 7) in moves
+
 def test_valid_position():
     board = BoardFactory.create_empty_board()
     white_bishop = Bishop(PieceColorEnum.WHITE, 'Bishop')
