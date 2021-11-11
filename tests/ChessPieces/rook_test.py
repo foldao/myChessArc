4cc85bb336dff8890791f6fc5aeca9957b61ead5
Fabrício <fabricio_passadore@hotmail.com>
2021-11-11 01:22:03 -0300

    just realised the piece is called rook in english

--- tests/ChessPieces/rook_test.py
+++ tests/ChessPieces/rook_test.py
@@ -1,14 +1,14 @@
 from src.Entities.chess.BoardFactory import BoardFactory
-from src.Entities.chess.ChessPieces.Tower import Tower
+from src.Entities.chess.ChessPieces.Rook import Rook
 from src.Entities.chess.PieceColorEnum import PieceColorEnum
 from src.Entities.chess.Position import Position
 
 
 def test_valid_position():
     board = BoardFactory.create_empty_board()
-    white_tower = Tower(PieceColorEnum.WHITE, 'Tower')
-    board.set_piece_by_indexes(3, 3, white_tower)
-    moves = white_tower.get_possible_moves(board, Position(3, 3))
+    white_Rook = Rook(PieceColorEnum.WHITE, 'Rook')
+    board.set_piece_by_indexes(3, 3, white_Rook)
+    moves = white_Rook.get_possible_moves(board, Position(3, 3))
     assert Position(3, 4) in moves
     assert Position(6, 3) in moves
     assert Position(4, 4) not in moves
