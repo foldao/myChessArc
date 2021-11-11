9ba4d434f4576dbd25e0878c17eb34fff46ae062
Fabrício <fabricio_passadore@hotmail.com>
2021-11-11 00:56:18 -0300

    empty position type hint corrections

--- src/Entities/chess/ChessPieces/EmptyPosition.py
+++ src/Entities/chess/ChessPieces/EmptyPosition.py
@@ -1,9 +1,11 @@
+from __future__ import annotations
 
-from typing import List
-from src.Entities.chess.Board import Board
+from typing import TYPE_CHECKING, List
 from src.Entities.chess.ChessPieces.AbstractPiece import AbstractPiece
 from src.Entities.chess.PieceColorEnum import PieceColorEnum
-from src.Entities.chess.Position import Position
+if TYPE_CHECKING:
+    from src.Entities.chess.Board import Board
+    from src.Entities.chess.Position import Position
 
 
 class EmptyPosition(AbstractPiece):
