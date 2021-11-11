d25066ebb0f9f0ba64bc17be3e41cd044ac2e6bb
Fabrício <fabricio_passadore@hotmail.com>
2021-11-11 03:24:31 -0300

    pawn movement bug fixes

--- src/Entities/chess/ChessPieces/Pawn.py
+++ src/Entities/chess/ChessPieces/Pawn.py
@@ -15,16 +15,16 @@ class Pawn(AbstractPiece):
 
     def get_possible_moves(self, board: bd.Board, position: Position):
         movements: List[Position] = []
-        if (front_position := Position.try_to_create(*(Position.tuple_sum(position.as_tuple(), (0, 1))))):
+        if (front_position := Position.try_to_create(*(Position.tuple_sum(position.as_tuple(), (0, self.color.value))))):
             match board.get_piece_by_position(front_position):
                 case EmptyPosition():
                     movements.append(front_position)
-        for diag_dir in [(1, 1), (-1, 1)]:
+        for diag_dir in [(1,  self.color.value), (-1, self.color.value)]:
             if diag_position := Position.try_to_create(*(Position.tuple_sum(position.as_tuple(), diag_dir))):
                 match board.get_piece_by_position(diag_position):
                     case EmptyPosition():
                         side_position = Position(
-                            *(Position.tuple_sum(diag_position.as_tuple(), (0, -1))))
+                            *(Position.tuple_sum(diag_position.as_tuple(), (0, -1*self.color.value))))
                         match piece := board.get_piece_by_position(side_position):
                             case Pawn(color=others_color):
                                 if others_color != self.color and piece.turn_when_can_get_taken_en_passant and side_position.y in [4, 5]:
